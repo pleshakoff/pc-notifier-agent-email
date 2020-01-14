67661ec3d3777c32436dfb811f41854721245c80
pleshakoff <pleshakoff@gmail.com>
2020-01-14 21:53:25 +0300

    Получение сообщений через кафку

--- src/main/java/com/parcom/notifier_agent_email/notification/NotificationService.java
+++ src/main/java/com/parcom/notifier_agent_email/notification/NotificationService.java
@@ -1,5 +1,7 @@
 package com.parcom.notifier_agent_email.notification;
 
+import com.parcom.asyncdto.NotificationAgentDto;
+
 public interface NotificationService {
-    void send(NotificationAgentDto notificationDto);
+    void send(NotificationAgentDto notificationAgentDto);
 }
--- src/main/java/com/parcom/notifier_agent_email/notification/NotificationServiceImpl.java
+++ src/main/java/com/parcom/notifier_agent_email/notification/NotificationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.parcom.notifier_agent_email.notification;
 
+import com.parcom.asyncdto.NotificationAgentDto;
 import com.parcom.notifier_agent_email.user.User;
 import com.parcom.notifier_agent_email.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +15,14 @@ public class NotificationServiceImpl implements NotificationService {
     private final UserService userService;
 
     @Override
-    public void send(NotificationAgentDto notificationDto) {
+    public void send(NotificationAgentDto notificationAgentDto) {
 
-        User user = userService.getById(notificationDto.getIdUserReciever());
+        User user = userService.getById(notificationAgentDto.getIdUserReceiver());
         if (user.getEmail() != null) {
 
             log.info("Send email to \"{}\" ",user.getEmail());
-            log.info("Type:  \"{}\" Title: \" {}\"",notificationDto.getNotificationType(),notificationDto.getTitle());
-            log.info("Message:  \"{}\"",notificationDto.getMessage());
+            log.info("Type:  \"{}\" Title: \" {}\"", notificationAgentDto.getNotificationType(), notificationAgentDto.getTitle());
+            log.info("Message:  \"{}\"", notificationAgentDto.getMessage());
         }
         else
             log.info("Can't send email for user id  {}. Email field is empty",user.getId());
